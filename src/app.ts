1b43125e900fdfe2296e2e04a4a558e1df70ed0e
Ismar Hadzic <hadzicismar67@gmail.com>
2022-08-14 15:32:07 +0200

    minor changes

--- src/app.ts
+++ src/app.ts
@@ -1,53 +1,57 @@
-import compression from 'compression';
-import cors from 'cors';
+import compression from 'compression'
+import cors from 'cors'
 import express, { json, urlencoded } from 'express'
-import { hidePoweredBy, xssFilter } from 'helmet';
-import { memory_limit } from './config/constants';
-import Logger from './services/logger';
-import i18n from "./config/localization";
-import initializeMongooseClient from './services/database';
-import { config } from 'dotenv';
-import { APIRouter } from './api';
-import { notFoundHandler, errorHandler } from './api/utility/response';
+import { hidePoweredBy, xssFilter } from 'helmet'
+import { memory_limit } from './config/constants'
+import Logger from './services/logger'
+import i18n from './config/localization'
+import initializeMongooseClient from './services/database'
+import { config } from 'dotenv'
+import { APIRouter } from './api'
+import { notFoundHandler, errorHandler } from './api/utility/response'
 
 // Init env
-config();
+config()
 
-const port = process.env.PORT !== undefined ? Number(process.env.PORT) : 3000;
-const logger = Logger("Express Application");
+const port = process.env.PORT !== undefined ? Number(process.env.PORT) : 3000
+const logger = Logger('Express Application')
 
 const app = express()
 
 app.use(hidePoweredBy())
 app.use(xssFilter())
-app.use(cors());
-app.use(json({limit: memory_limit}));
-app.use(compression());
-app.use(urlencoded({limit: memory_limit, extended: false}))
+app.use(cors())
+app.use(json({ limit: memory_limit }))
+app.use(compression())
+app.use(urlencoded({ limit: memory_limit, extended: false }))
 app.use(i18n.init)
 
+const mountDocs = process.env.MOUNT_DOCS !== 'false'
 
-const mountDocs = process.env.MOUNT_DOCS !== "false";
-
-APIRouter.mountRoutes(app, "v1", mountDocs);
+APIRouter.mountRoutes(app, 'v1', mountDocs)
 
 if (mountDocs) {
-    APIRouter.mountSwaggerUI(app, ["v1"]);
+  APIRouter.mountSwaggerUI(app, ['v1'])
 }
 
-app.use(notFoundHandler);
-app.use(errorHandler);
-
+app.use(notFoundHandler)
+app.use(errorHandler)
 
 app.listen(port, () => {
-    logger.info(`App is up and running on port http://localhost:${port}`)
+  logger.info(`App is up and running on port http://localhost:${port}`)
 
-    initializeMongooseClient(process.env.NODE_ENV === "test")
-        .then(() => {
-            logger.info("Database connection is established successfully.")
-        }, (error) => {
-            logger.error("Failed initializing database connection.", error);
-        });
+  if (mountDocs) {
+    logger.info(
+      `Documentation is served on http://localhost:${port}/services/docs`
+    )
+  }
+
+  initializeMongooseClient(process.env.NODE_ENV === 'test').then(
+    () => {
+      logger.info('Database connection is established successfully.')
+    },
+    (error) => {
+      logger.error('Failed initializing database connection.', error)
+    }
+  )
 })
-
-
